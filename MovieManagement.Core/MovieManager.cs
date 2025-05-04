b2a3726008fcd4c88f5201cd86ff559238b08505
Cvilla142 <villanueva_josh@outlook.com>
2025-05-04 23:42:53 +1200

    1.1 Movie Manager Skeleton
    I've added all the methods I will need within my MovieManager class

--- MovieManagement.Core/MovieManager.cs
+++ MovieManagement.Core/MovieManager.cs
@@ -2,5 +2,38 @@ namespace MovieManagement.Core;
 
 public class MovieManager
 {
+    private LinkedList<Movie> movies = new();
 
+    private Dictionary<string, Movie> movieLookup = new();
+
+    public void Add AddMovie(Movie movie) 
+    {
+    }
+    public void RemoveMovie(Movie movie)
+    {
+    }
+    
+    public Movie>? SearchByID(string movieID) { return null; }
+
+    public List<Movie> SearchByTitle(string title) { return new List<Movie>(); }
+
+    public void SortByTitle()
+    {
+    }
+
+    public void BorrowMovie(string movieID, string userID) 
+    {         
+    }
+
+    public void ReturnMovie(string filepath)
+    {        
+    }
+
+    public void ExportToJson(string filepath)
+    {        
+    }
+
+    public void ImportToJson(string filepath,)
+    {       
+    }
 }
