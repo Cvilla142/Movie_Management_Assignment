3a0935be094c6d929d2766d1a2e7a77b942b8cef
Cvilla142 <villanueva_josh@outlook.com>
2025-05-04 23:15:16 +1200

    1.0 Creation of Movie Class
    I created the movie classes with the relevant constructors

--- MovieManagement.Core/Movie.cs
+++ MovieManagement.Core/Movie.cs
@@ -1,6 +1,11 @@
 ﻿namespace MovieManagement.Core;
 
-public class Class1
+public class Movie
 {
-
+    public string MovieId { get; set;}
+    public string MovieTitle { get; set;}
+    public string Director { get; set;}
+    public string Genre { get; set;}
+    public int ReleaseYear { get; set;}
+    public bool IsAvailable { get; set;}
 }
