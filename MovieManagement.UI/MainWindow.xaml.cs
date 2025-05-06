717f378a8dc739405ec0784882d9512763fe3698
Cvilla142 <villanueva_josh@outlook.com>
2025-05-06 22:17:02 +1200

    1.0 UI creation
    Created the first UI solution. Very basic with changes in the MainWindow xaml and xaml.cs files. I've also made changes in the UI.csproj file to allow Json

--- MovieManagement.UI/MainWindow.xaml.cs
+++ MovieManagement.UI/MainWindow.xaml.cs
@@ -1,23 +1,60 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.Win32;
+using MovieManagement.Core;
 
-namespace MovieManagement.UI;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace MovieManagement.UI
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        private readonly MovieManager _manager = new MovieManager();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RefreshGrid();
+        }
+
+        private void RefreshGrid()
+        {
+            MoviesGrid.ItemsSource = null;
+            MoviesGrid.ItemsSource = _manager.GetAllMovies();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            var results = _manager.SearchByTitle(SearchBox.Text);
+            MoviesGrid.ItemsSource = results;
+        }
+
+        private void SortTitle_Click(object sender, RoutedEventArgs e)
+        {
+            _manager.SortByTitle();
+            RefreshGrid();
+        }
+
+        private void SortYear_Click(object sender, RoutedEventArgs e)
+        {
+            _manager.SortByReleaseYear();
+            RefreshGrid();
+        }
+
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog { Filter = "JSON Files (*.json)|*.json" };
+            if (dlg.ShowDialog() == true)
+            {
+                _manager.ImportFromJson(dlg.FileName);
+                RefreshGrid();
+            }
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new SaveFileDialog { Filter = "JSON Files (*.json)|*.json" };
+            if (dlg.ShowDialog() == true)
+            {
+                _manager.ExportToJson(dlg.FileName);
+            }
+        }
     }
 }
